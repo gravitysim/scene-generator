1a6e81436d76daf57304efc04b42502d90a24346
k.chaykin <chaykin.k@gmail.com>
2021-03-27 17:56:45 +0500

    Initial code commit

---
+++ circleOrbit.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	gravMath "github.com/gravitysim/gravity-commons/math"
+	"github.com/gravitysim/gravity-commons/phys"
+	"github.com/gravitysim/gravity-commons/scene"
+	"math"
+)
+
+func createCircleOrbitScene() *scene.GravityScene {
+	centralBody := phys.NewBody("centralBody", 5e24, 6400e3, gravMath.Point{}, gravMath.Vector{})
+	satBody := phys.NewBody("satBody", 1, 1, gravMath.Point{X: 6500e3, Y: 0}, gravMath.Vector{})
+	satBody.Velocity.Y = circleVelocity(centralBody.GetMass(), satBody.Position.X)
+
+	gravScene := scene.GravityScene{}
+	gravScene.AddBody(&centralBody)
+	gravScene.AddBody(&satBody)
+
+	return &gravScene
+}
+
+func circleVelocity(mass, dst float64) float64 {
+	return math.Sqrt(phys.G * mass / dst)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gravitysim/scene-generator
+
+go 1.16
+
+require github.com/gravitysim/gravity-commons v1.0.0
